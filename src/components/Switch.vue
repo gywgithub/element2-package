7cc29d183ed6d98f5c19768d6c96bab46e25959d
yanjingjing <yanjj@icbf.com.cn>
2022-04-01 16:48:06 +0800

    add select、inputNumber、switch、slider

---
+++ src/components/Switch.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-switch
+      v-model="config.value"
+      :disabled="config.disabled"
+      :width="config.width"
+      :active-icon-class="config.activeIconClass"
+      :inactive-icon-class="config.inactiveIconClass"
+      :active-text="config.activeText"
+      :inactive-text="config.inactiveText"
+      :active-value="config.activeValue"
+      :inactive-value="config.inactiveValue"
+      :active-color="config.activeColor"
+      :inactive-color="config.inactiveColor"
+      :name="config.name"
+      :validate-event="config.validateEvent"
+      v-on="$listeners"
+    >
+    </el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SwitchUI",
+  props: {
+    config: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
---
+++ src/components/select/Option.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <el-option
+        :value="content.value"
+        :label="content.label"
+        :disabled="content.disabled"
+      >
+        <slot></slot>
+      </el-option>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Option',
+  props: {
+    content: {
+      type: Object,
+      default: () => {}
+    }
+  },
+}
+</script>
