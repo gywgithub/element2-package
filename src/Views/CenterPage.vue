5d2469b771d20e01d1ca358d172884ba423f6a4d
gywgithub <qingyi_w@outlook.com>
2022-04-13 13:43:05 +0800

    add table

--- src/Views/CenterPage.vue
+++ src/Views/CenterPage.vue
@@ -15,6 +15,58 @@
       @change="colorPickerHandleChange"
       v-model="colorPicker.config.value"
     ></color-picker>
+    <h2>Table 表格01</h2>
+    <cbf-table :config="table.config" :data="table.data">
+      <cbf-table-column :config="table.config.columns[0]"></cbf-table-column>
+      <cbf-table-column :config="table.config.columns[1]"></cbf-table-column>
+      <cbf-table-column :config="table.config.columns[2]"></cbf-table-column>
+    </cbf-table>
+    <h2>Table 表格02</h2>
+    <cbf-table :config="table2.config" :data="table2.data">
+      <cbf-table-column :config="table2.config.columns[0]"></cbf-table-column>
+      <cbf-table-column :config="table2.config.columns[1]"></cbf-table-column>
+      <cbf-table-column :config="table2.config.columns[2]"> </cbf-table-column>
+      <cbf-table-column :config="table2.config.columns[3]">
+        <template slot="custom" slot-scope="nativeData">
+          <el-button
+            @click="handleSelectClick(nativeData)"
+            type="text"
+            size="small"
+            >查看</el-button
+          >
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </cbf-table-column>
+    </cbf-table>
+
+    <h2>Table 表格03 - 自定义列模板</h2>
+    <cbf-table :config="table3.config" :data="table2.data">
+      <cbf-table-column :config="table3.config.columns[0]">
+        <template slot="custom" slot-scope="{ nativeData }">
+          <span style="margin-left: 10px">{{ nativeData[2] }}</span>
+        </template>
+      </cbf-table-column>
+
+      <cbf-table-column :config="table3.config.columns[1]">
+        <template slot="custom" slot-scope="{ nativeData }">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ nativeData[0].date }}</span>
+        </template>
+      </cbf-table-column>
+      <cbf-table-column :config="table3.config.columns[2]"></cbf-table-column>
+      <cbf-table-column :config="table3.config.columns[3]"> </cbf-table-column>
+      <cbf-table-column :config="table3.config.columns[4]">
+        <template slot="custom" slot-scope="{ nativeData }">
+          <el-button
+            @click="handleSelectClick(nativeData)"
+            type="primary"
+            size="small"
+            >查看</el-button
+          >
+          <el-button type="danger" size="small">删除</el-button>
+        </template>
+      </cbf-table-column>
+    </cbf-table>
   </div>
 </template>
 
@@ -22,12 +74,18 @@
 import Upload from '../components/Upload.vue'
 import Rate from '../components/Rate.vue'
 import ColorPicker from '../components/ColorPicker.vue'
+// Table
+import CbfTable from '../components/table/Index.vue'
+import CbfTableColumn from '../components/table/TableColumn.vue'
+
 export default {
   name: '',
   components: {
     Upload,
     Rate,
-    ColorPicker
+    ColorPicker,
+    CbfTable,
+    CbfTableColumn
   },
   props: {
   },
@@ -71,6 +129,150 @@ export default {
           value: "#409EFF",
         },
       },
+
+      table: {
+        config: {
+          // defaultSort: { order: 'descending' },
+          columns: [
+            {
+              prop: 'date',
+              label: '日期',
+              order: 2
+            },
+            {
+              prop: 'name',
+              label: '姓名',
+              order: 1
+            },
+            {
+              prop: 'address',
+              label: '地址',
+              order: 3
+            }
+          ]
+        },
+        data: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      },
+      table2: {
+        config: {
+          columns: [
+            {
+              prop: 'date',
+              label: '日期',
+              width: '200',
+              key: 'date',
+            },
+            {
+              prop: 'name',
+              label: '姓名',
+              key: 'name',
+            },
+            {
+              prop: 'address',
+              label: '地址',
+              width: '400',
+              key: 'address',
+            },
+            {
+              label: '操作',
+              width: '100',
+              fixed: 'right',
+            }
+          ],
+          border: true,
+          stripe: true,
+          size: 'mini'
+        },
+        data: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      },
+
+      table3: {
+        config: {
+          columns: [
+            {
+              width: '50',
+              type: 'index'
+            },
+            {
+              prop: 'date',
+              label: '日期',
+              width: '200',
+              key: 'date',
+              sortable: true,
+              isNativeRenter: true
+            },
+            {
+              prop: 'name',
+              label: '姓名',
+              key: 'name',
+              sortable: true
+            },
+            {
+              prop: 'address',
+              label: '地址',
+              width: '400',
+              key: 'address'
+            },
+            {
+              label: '操作',
+              width: '200',
+              fixed: 'right',
+            }
+          ],
+          border: true,
+          stripe: true,
+          size: 'mini'
+        },
+        data: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
     }
   },
   methods: {
@@ -82,6 +284,9 @@ export default {
     colorPickerHandleChange (value) {
       console.log("color picker chagne ", value);
     },
+    handleSelectClick (data) {
+      console.log(data)
+    }
   }
 }
 </script>
---
+++ src/components/table/TableColumn.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- v-model="config.value" -->
+  <el-table-column
+    v-bind="$attrs"
+    :type="config.type"
+    :index="config.index"
+    :column-key="config.columnKey"
+    :label="config.label"
+    :prop="config.prop"
+    :width="config.width"
+    :min-width="config.minWidth"
+    :fixed="config.fixed"
+    :render-header="config.renderHeader"
+    :sortable="config.sortable"
+    :sort-method="config.sortMethod"
+    :sort-by="config.sortBy"
+    :sort-orders="config.sortOrders"
+    :resizable="config.resizeable"
+    :formatter="config.formatter"
+    :show-overflow-tooltip="config.showOverflowTooltip"
+    :align="config.align"
+    :header-align="config.headerAlign"
+    :class-name="config.className"
+    :label-class-name="config.labelClassName"
+    :selectable="config.selectable"
+    :reserve-selection="config.reserveSelection"
+    :filters="config.filters"
+    :filter-placement="config.filterPlacement"
+    :filter-multiple="config.filterMultiple"
+    :filter-method="config.filterMethod"
+    :filtered-value="config.filteredValue"
+    v-on="$listeners"
+  >
+    <template slot-scope="{ row, column, $index }">
+      <slot :nativeData="[row, column, $index]" name="custom"></slot>
+      <span v-if="!config.isNativeRenter">{{ row[column.property] }}</span>
+    </template>
+    <!-- <template slot-scope="scope" >
+      <slot :nativeData="scope" name="custom"></slot>
+      {{scope.row[scope.column.property]}}
+    </template> -->
+    <template slot="header">
+      <slot name="header"></slot>
+    </template>
+  </el-table-column>
+</template>
+
+<script>
+export default {
+  name: 'CBFTableColumn',
+  props: {
+    content: {
+      type: String,
+      default: ''
+    },
+    config: {
+      type: Object,
+      default: () => { }
+    }
+  }
+}
+</script>
