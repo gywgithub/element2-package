16ccff164bed10d043fe5b5b9f15e9d0e67b2638
yanjingjing <yanjj@icbf.com.cn>
2022-04-21 10:56:54 +0800

    add collapse card

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-card
+      :header="config.header"
+      :body-style="config.bodyStyle"
+      :shadow="config.shadow"
+      v-on="$listeners">
+      <slot></slot>  
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    config: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
---
+++ src/components/Collapse/CollapseItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-collapse-item
+      :name="content.name"
+      :title="content.title"
+      :disabled="content.disabled"
+      v-on="$listeners">
+      <template slot="title">
+        <slot name="title"></slot>
+      </template>
+      <slot></slot>
+    </el-collapse-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapseItem',
+  props: {
+    content: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
