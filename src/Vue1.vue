245476b9d768a6a8d457df48d19d5cb20756eed5
yanjingjing <yanjj@icbf.com.cn>
2022-04-21 10:59:07 +0800

    update collapse,add carousel

--- src/Vue1.vue
+++ src/Vue1.vue
@@ -125,9 +125,9 @@
       :config="card.config">
       <div>Card卡片</div>
     </Card><h2 class="mt10">Collapse折叠面板</h2>
-    <cbf-collapse
+    <Collapse
       :config="collapse.config">
-      <cbf-collapse-item :content="collapse.content[0]">
+      <Collapse-item :content="collapse.content[0]">
         <template #title>
           一致性 Consistency<i class="header-icon el-icon-info"></i>
         </template>
@@ -135,27 +135,35 @@
           <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
           <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
         </template>
-      </cbf-collapse-item>
-      <cbf-collapse-item :content="collapse.content[1]">
+      </Collapse-item>
+      <Collapse-item :content="collapse.content[1]">
         <template>
           <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
           <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
         </template>
-      </cbf-collapse-item>  
-      <cbf-collapse-item :content="collapse.content[2]">
+      </Collapse-item>  
+      <Collapse-item :content="collapse.content[2]">
         <template>
           <div>简化流程：设计简洁直观的操作流程；</div>
           <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
           <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
         </template>
-      </cbf-collapse-item>  
-      <cbf-collapse-item :content="collapse.content[3]">
+      </Collapse-item>  
+      <Collapse-item :content="collapse.content[3]">
         <template>
           <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
           <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
         </template>
-      </cbf-collapse-item>  
-    </cbf-collapse>
+      </Collapse-item>  
+    </Collapse>
+    <h2 class="mt10">Carousel走马灯</h2>
+    <Carousel
+      :config="carousel.config"
+      :content="carousel.content">
+      <template slot-scope="scope">
+        <p>{{scope.item.label}}</p>
+      </template>
+    </Carousel>
   </div>
 </template>
 
@@ -179,6 +187,7 @@ import Popconfirm from "./components/Popconfirm.vue";
 import Card from "./components/Card.vue";
 import Collapse from "./components/Collapse/Collapse.vue";
 import CollapseItem from "./components/Collapse/CollapseItem.vue";
+import Carousel from "./components/Carousel.vue";
 
 
 export default {
@@ -202,7 +211,8 @@ export default {
     Popconfirm,
     Card,
     Collapse,
-    CollapseItem
+    CollapseItem,
+    Carousel
   },
   data() {
     return {
@@ -563,6 +573,31 @@ export default {
           shadow: 'hover'
         }
       },
+      // Collapse折叠面板
+      collapse: {
+        config: {
+          accordion: true,
+        },
+        content: [
+          { title: '测试1', name: '测试1', disabled: true },
+          { title: '测试2', name: '测试2' },
+          { title: '测试3', name: '测试3' },
+          { title: '测试4', name: '测试4' },
+        ]
+      },
+      // Carousel走马灯
+      carousel: {
+        config: {
+          height: '100px',
+          autoplay: false
+        },
+        content: [
+          { name: '测试1', label: '测试1label' },
+          { name: '测试2', label: '测试2label' },
+          { name: '测试3', label: '测试3label' },
+          { name: '测试4', label: '测试4label' },
+        ]
+      }
     };
   },
   methods: {
---
+++ src/components/Carousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-carousel
+      :height="config.height"
+      :initial-index="config.initialIndex"
+      :trigger="config.trigger"
+      :autoplay="config.autoplay"
+      :interval="config.interval"
+      :indicator-position="config.indicatorPosition"
+      :arrow="config.arrow"
+      :type="config.type"
+      :loop="config.loop"
+      :direction="config.direction"
+      v-on="$listeners">
+      <el-carousel-item v-for="(item, index) in content" :key="index">
+        <template>
+          <slot :item="item"></slot>
+        </template>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  props: {
+    config: {
+      type: Object,
+      default: () => {}
+    },
+    content: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
